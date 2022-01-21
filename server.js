201070ef612382f84c33f531aded13f8e9b0c083
JG HAN <gojkhan13@gmail.com>
2022-01-21 16:37:28 +0900

    0121 mongodb connection setting

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require('express'); //express라는 라이브를 첨부함
 const app = express(); // express라이브를 이용해서 새로운 객체 생성
+const bodyParser = require('body-parser');
+const { response } = require('express');
+app.use(bodyParser.urlencoded({extended : true}));
 
-app.listen(8080, function(){
-    console.log('test~!!')
-}); //서버를 어디서 열지 결정, listen(서버 띄울 포트번호, 띄운 후 실행할 코드)
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb+srv://admin:3shan212406@cluster0.nuqju.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+                    function(error, client){
 
-app.get('/pet', function(req, rep){
-    rep.send('펫용품 사이트잉ㅂ니다.');
+    app.listen(8080, function(){
+        console.log('listening on 8080')
+    }); //서버를 어디서 열지 결정, listen(서버 띄울 포트번호, 띄운 후 실행할 코드)
+
+})
+
+
+
+
+
+//함수 안의 함수(function(){}): 콜백함수, 순차적으로 실행하고플때 씀
+app.get('/', function(req, rep){
+    rep.sendFile(__dirname+'/index.html');
+});
+
+app.get('/write', function(req, rep){
+    rep.sendFile(__dirname+ '/write.html')
+});
+
+app.post('/add', function(req, rep){
+    rep.send('전송완료');
+    console.log(req.body.title)
+
 });
