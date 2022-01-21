e12c5d21979e811b2ecd0f742b90801a3637db04
JongGurlHan <gojkhan13@gmail.com>
2022-01-22 01:32:24 +0900

    0122 dbupdate함수, inc연산자

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ MongoClient.connect('mongodb+srv://admin:3shan212406@cluster0.nuqju.mongodb.net/
 
     db = client.db('todoapp'); //todoapp 이라는 database(폴더)에 연결
 
-    app.listen(8080, function(){ //서버를 어디서 열지 결정, listen(서버 띄울 포트번호, 띄운 후 실행할 코드)
-        console.log('listening on 8080')
+    app.listen(8090, function(){ //서버를 어디서 열지 결정, listen(서버 띄울 포트번호, 띄운 후 실행할 코드)
+        console.log('listening on 8090')
     }); 
 
 })
@@ -31,17 +31,43 @@ app.get('/write', function(req, rep){
     rep.sendFile(__dirname+ '/write.html')
 });
 
+// 1. /add로 post 요청하면(폼 전송하면) 
 app.post('/add', function(req, rep){
     rep.send('전송완료');
-    db.collection('post').insertOne({제목 : req.body.title, 날짜: req.body.date}, function(err, rst){ //Object 자료형으로 저장
-        console.log('저장완료');
-    });
+
+    //2. DB.counter 내의 총게시물 갯수를 찾음
+    db.collection('counter').findOne({name: '게시물갯수'}, function(err, rst){
+        console.log(rst.totalPost); 
+        var 총게시물갯수 = rst.totalPost; //3. 총게시물 갯수를 변수에 저장
+
+        //4. 이제 DB.post에 새 게시물 저장
+        db.collection('post').insertOne({ _id : 총게시물갯수 + 1, 제목 : req.body.title, 날짜: req.body.date}, function(err, rst){ //Object 자료형으로 저장
+            console.log('저장완료');
+
+            //5. 저장이 완료되면 db.counter내의 총 게시물 갯수+1
+            // $inc(operator): {$inc : {totalPost: 기존값에 더해줄 값}}
+            db.collection('counter').updateOne({name: '게시물갯수'},{ $inc : {totalPost:1} }, function(err, rst){
+                if(err){return console.log(err)}
+            }) 
+
+        });
+
+
+    }); 
+    
     console.log(req.body.title)
 
 });
 
 
 app.get('/list', function(req, rep){
-    rep.render('list.ejs')
+    
+    // 디비에 저장된 post라는 collection안의 모든 데이터를 꺼내주세요(.find().toArray();)
+    db.collection('post').find().toArray(function(err, rst){
+        console.log(rst);
+        rep.render('list.ejs', {posts : rst}); //마치 model.addAttribute와 같은
+    });
+
+
 
 });
